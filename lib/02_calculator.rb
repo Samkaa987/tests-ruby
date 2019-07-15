1b7bec3189e07fb8f2d786023de6bbb39a8acac4
Samkaa987 <deneubourg.samuel@gmail.com>
2019-07-15 12:16:56 +0200

    calculator done but need modif

---
+++ lib/02_calculator.rb
@@ -0,0 +1,53 @@
+def add(num1, num2)
+	return num1 + num2
+	
+end
+
+
+
+
+
+
+
+
+def subtract(num3, num4)
+	return num3 - num4
+	
+end
+
+
+
+
+
+
+
+
+def sum(array)
+	summ = 0
+	array.each do |i|
+		summ += i
+		
+	end
+	return summ
+	
+end
+
+
+
+
+
+
+def multiply(num5, num6)
+	return num5 * num6
+	
+end
+
+
+
+
+
+
+def power(num7, num8)
+	return num7 ** num8
+	
+end
--- spec/02_calculator_spec.rb
+++ spec/02_calculator_spec.rb
@@ -47,16 +47,24 @@ describe "#multiply" do
     expect(multiply(3, 4)).to eq(12)
   end
 
-  it "multiplies two other numbers"
+  it "multiplies two other numbers" do
+    expect(multiply(6, 5)).to eq(30)
     # one other test here, don't forget do end ;)
+  end
 
-  it "multiplies two other numbers, one of them being 0"
+  it "multiplies two other numbers, one of them being 0" do
+    expect(multiply(0, 30)).to eq(0)
     # one last test, with 0 in it
+  end
 
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+
+  it "raises one number to the power of another number" do
+    expect(power(5, 3)).to eq(125)
+  end
+  
 end
 
 # http://en.wikipedia.org/wiki/Factorial
